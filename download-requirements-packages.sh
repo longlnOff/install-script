0590f00e262fb7f7e58ff5c33e5bbeb50ffcb880
long <longlnofficial@gmail.com>
2023-09-04 15:46:43 +0700

    update download software

---
+++ download-requirements-packages.sh
@@ -0,0 +1,3 @@
+wget -P 'software' 'https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_116.0.1938.69-1_amd64.deb?brand=M102'
+wget -P 'software' 'https://az764295.vo.msecnd.net/stable/6c3e3dba23e8fadc360aed75ce363ba185c49794/code_1.81.1-1691620686_amd64.deb'
+wget -P 'software' 'https://download.anydesk.com/linux/anydesk_6.3.0-1_amd64.deb'
